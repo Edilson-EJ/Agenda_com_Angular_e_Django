e31114b069e423b6ca6af47139b8c539ef64289f
Edilson-EJ <edilsonjunior624@gmail.com>
2023-09-08 16:46:34 -0300

    first commit

--- Agenda/src/app/meu-formulario/meu-formulario.component.ts
+++ Agenda/src/app/meu-formulario/meu-formulario.component.ts
@@ -1,26 +1,46 @@
 import { Component } from '@angular/core';
 import { Item } from './../item.model';
 
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
 @Component({
   selector: 'app-meu-formulario',
   templateUrl: './meu-formulario.component.html',
   styleUrls: ['./meu-formulario.component.css']
 })
 export class MeuFormularioComponent {
+
+    private baseURL = '127.0.0.1:8000/bancoAgenda/'
+
+    constructor(private http: HttpClient){}
+
     itens: Item[] = [];
     novoItem: Item = new Item ('','', '', '', '');
-  
-    adicionarItem(): void {
-      this.itens.push(this.novoItem);
-      this.novoItem = new Item('', '', '', '', ''); // Limpa o novoItem
-    }
-  
-    excluirItem(index: number): void {
-      this.itens.splice(index, 1);
-  }
 
-  concluirTarefa(){
-    const element = document.querySelector('li')
-    element?.classList.add("riscaTarefa")    
-  }
+    ListarAgendamento(){
+      return this.http.get(this.baseURL)
+    }
+
+    criarAgendamento(Item: any) {
+      return this.http.post(this.baseURL + 'create/', Item);
+    }
+
+    atualizarAgendamento(index: number, Item: any) {
+      return this.http.put(this.baseURL + `update/${index}/`,Item);
+    }
+
+    excluirAgendamento(index: number) {
+      return this.http.delete(this.baseURL + `delete/${index}/`);
+    }
+
+    //pega as informações digitadas pelo usuario salba no array
+    //
+
+
+
 }
